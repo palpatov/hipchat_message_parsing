f9b9fff944b22d9de076a870c4ff802136886a11
Phillip A Alpatov <phillipalpatov@Phillips-MacBook-Pro.local>
2016-08-17 23:47:20 -0500

    split the query processing into separate go-routines due to potential blocking on remote resource.

--- app/MessageParser.go
+++ app/MessageParser.go
@@ -3,6 +3,8 @@ package app
 import (
 	"io"
 
+	"sync"
+
 	"github.com/palpatov/hipchat_message_parsing/domain"
 	"github.com/palpatov/hipchat_message_parsing/hcjson"
 	"github.com/palpatov/hipchat_message_parsing/parsers"
@@ -14,18 +16,34 @@ import (
 func Parse(m string, w io.Writer) {
 	var result domain.ParseResult
 
-	me := parsers.ParseMentions(m)
-	if me != nil {
-		result.Mentions = me.MentionNames
-	}
-	em := parsers.ParseEmoticons(m)
-	if em != nil {
-		result.Emoticons = em.Emoticons
-	}
-	li := parsers.ParseUrls(m)
-	if li != nil {
-		result.Links = li.URLs
-	}
+	var wg sync.WaitGroup
+	wg.Add(2)
 
+	//
+	// splitting this in two groutines due to the fact that ParseUrls parser fetches
+	// remote resource (page). Need to think of a way to terminate if not aquired in
+	// some reasonable time interval
+	//
+	go func() {
+		defer wg.Done()
+		li := parsers.ParseUrls(m)
+		if li != nil {
+			result.Links = li.URLs
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		me := parsers.ParseMentions(m)
+		if me != nil {
+			result.Mentions = me.MentionNames
+		}
+		em := parsers.ParseEmoticons(m)
+		if em != nil {
+			result.Emoticons = em.Emoticons
+		}
+	}()
+
+	wg.Wait()
 	hcjson.FormatJsonToResponse(result, w)
 }
--- parsers/remote_web_parser.go
+++ parsers/remote_web_parser.go
@@ -3,7 +3,9 @@ package parsers
 import "github.com/palpatov/hipchat_message_parsing/fetchers"
 
 func WebPageGetTitle(url string) string {
-	//todo fill in the remote page title fetching here
+
+	//todo what do we do for long running fetches?
+	//we need some non blocking way of doing this
 	pagesrc := fetchers.FetchPage(url)
 	if len(pagesrc) > 0 {
 		return ParseTitle(pagesrc)
