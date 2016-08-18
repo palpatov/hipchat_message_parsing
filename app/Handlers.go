42f5ccf817e3c891df868ab536c6d6e1c06375a8
Phillip A Alpatov <phillipalpatov@Phillips-MacBook-Pro.local>
2016-08-18 12:17:45 -0500

    Index page now has usage examples

--- app/Handlers.go
+++ app/Handlers.go
@@ -11,7 +11,13 @@ import (
 //
 func Index(w http.ResponseWriter, r *http.Request) {
 	//todo add instructions to use the service (?Readme.md)
-	fmt.Fprintln(w, "Welcome!")
+	fmt.Fprintln(w, `Welcome to hipchat_message_parsing service. See below for usage examples
+
+    curl -X POST -H 'Content-Type: application/json' -d "{\"message\": \"Olympics are starting soon; http://www.nbcolympics.com\"}" http://localhost:8080/hipchat_message_parsing
+
+    curl -X POST -H 'Content-Type: application/json' -d "{\"message\": \"Good morning! (megusta) (coffee)\"}" http://localhost:8080/hipchat_message_parsing
+
+    curl -X POST -H 'Content-Type: application/json' -d "{\"message\": \"@bob @john (success) such a cool feature; https://twitter.com/jdorfman/status/430511497475670016\"}" http://localhost:8080/hipchat_message_parsing`)
 }
 
 type request_struct struct {
